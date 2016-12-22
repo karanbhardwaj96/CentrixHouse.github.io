def4beeceae079b461e3a37e4dccfc10ca2ed690
Mohit Srivastava <mohit.srivastava@openmailbox.org>
2016-12-22 15:55:12 +0530

    Fix everythin
    Signed-off-by: Mohit Srivastava <mohit.srivastava@openmailbox.org>

--- gulpfile.js
+++
@@ -1,9 +0,0 @@
-let gulp = require('gulp');
-let webserver = require('gulp-webserver');
-
-gulp.task('webserver', function () {
-  return gulp.src('')
-    .pipe(webserver({
-      livereload: true
-    }));
-});
